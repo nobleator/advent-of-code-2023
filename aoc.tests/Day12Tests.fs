47549ff75697e6294b0641427f96e5c362ae2ad9
nobleator <bn2471f9d@gmail.com>
2023-12-13 22:51:38 -0500

    Day 12 part 2

--- aoc.tests/Day12Tests.fs
+++ aoc.tests/Day12Tests.fs
@@ -13,39 +13,79 @@ let sample = @"???.### 1,1,3
 [<Fact>]
 let ``Day 12-1 sample`` () =
     let res = Day12.part1 sample
-    Assert.Equal(21, res)
+    Assert.Equal(21L, res)
 
 [<Fact>]
 let ``Day 12-1 sub-sample 1`` () =
     let res = Day12.part1 @"???.### 1,1,3"
-    Assert.Equal(1, res)
+    Assert.Equal(1L, res)
 
 [<Fact>]
 let ``Day 12-1 sub-sample 2`` () =
     let res = Day12.part1 @".??..??...?##. 1,1,3"
-    Assert.Equal(4, res)
+    Assert.Equal(4L, res)
 
 [<Fact>]
 let ``Day 12-1 sub-sample 3`` () =
     let res = Day12.part1 @"?#?#?#?#?#?#?#? 1,3,1,6"
-    Assert.Equal(1, res)
+    Assert.Equal(1L, res)
 
 [<Fact>]
 let ``Day 12-1 sub-sample 4`` () =
     let res = Day12.part1 @"????.#...#... 4,1,1"
-    Assert.Equal(1, res)
+    Assert.Equal(1L, res)
 
 [<Fact>]
 let ``Day 12-1 sub-sample 5`` () =
     let res = Day12.part1 @"????.######..#####. 1,6,5"
-    Assert.Equal(4, res)
+    Assert.Equal(4L, res)
 
 [<Fact>]
 let ``Day 12-1 sub-sample 6`` () =
     let res = Day12.part1 @"?###???????? 3,2,1"
-    Assert.Equal(10, res)
+    Assert.Equal(10L, res)
 
 [<Fact>]
 let ``Day 12-1 minimal`` () =
     let res = Day12.part1 @"#?? 1,1"
-    Assert.Equal(1, res)
+    Assert.Equal(1L, res)
+
+[<Fact>]
+let ``Day 12-2 sample`` () =
+    let res = Day12.part2 sample
+    Assert.Equal(525152L, res)
+
+[<Fact>]
+let ``Day 12-2 sub-sample 1`` () =
+    let res = Day12.part2 @"???.### 1,1,3"
+    Assert.Equal(1L, res)
+
+[<Fact>]
+let ``Day 12-2 sub-sample 2`` () =
+    let res = Day12.part2 @".??..??...?##. 1,1,3"
+    Assert.Equal(16384L, res)
+
+[<Fact>]
+let ``Day 12-2 sub-sample 3`` () =
+    let res = Day12.part2 @"?#?#?#?#?#?#?#? 1,3,1,6"
+    Assert.Equal(1L, res)
+
+[<Fact>]
+let ``Day 12-2 sub-sample 4`` () =
+    let res = Day12.part2 @"????.#...#... 4,1,1"
+    Assert.Equal(16L, res)
+
+[<Fact>]
+let ``Day 12-2 sub-sample 5`` () =
+    let res = Day12.part2 @"????.######..#####. 1,6,5"
+    Assert.Equal(2500L, res)
+
+[<Fact>]
+let ``Day 12-2 sub-sample 6`` () =
+    let res = Day12.part2 @"?###???????? 3,2,1"
+    Assert.Equal(506250L, res)
+
+[<Fact>]
+let ``Day 12-2 minimal`` () =
+    let res = Day12.part2 @"#?? 1,1"
+    Assert.Equal(1L, res)
--- aoc/Day12.fs
+++ aoc/Day12.fs
@@ -1,4 +1,13 @@
 module Day12
+    let unfoldInput springs groups  =
+        let newSprings = [0..4] |> List.collect (fun n ->
+            match n with
+            | 4 -> springs
+            | _ -> springs @ ['?']
+        )
+        let newGroups = [|0..4|] |> Array.collect (fun _ -> groups)
+        (newSprings, newGroups)
+    
     let parseInput (str : string) =
         str.Split "\n"
         |> Array.map (fun x ->
@@ -15,38 +24,56 @@ module Day12
         | Some _ -> List.skip skipSize skipList
         | None -> []
 
-    let rec check springs groups =
-        match springs with
-        | h::t ->
-            match Array.length groups = 0 with
-            | true ->
-                match List.contains '#' springs with
-                | true -> 0
-                | false -> 1
-            | false -> 
-                let groupSize = Array.head groups
-                let validStretch =
-                    groupSize <= List.length springs &&
-                    List.take groupSize springs |> List.forall (fun c -> c <> '.') &&
-                    (groupSize = List.length springs || List.item groupSize springs <> '#')
-                match h with
-                | '.' -> check t groups
-                | '?' when not validStretch-> check t groups
-                | '?' when validStretch -> check t groups + check (trySkipWithDefault (groupSize + 1) springs) (Array.tail groups)
-                | '#' when not validStretch -> 0
-                | '#' when validStretch -> check (trySkipWithDefault (groupSize + 1) springs) (Array.tail groups)
-                | _ -> failwith "Springs should always match . ? or #"
-        | [] ->
-            match Array.length groups = 0 with
-            | true -> 1
-            | false -> 0
+    let cache = new System.Collections.Generic.Dictionary<System.Tuple<string, string>, int64>(HashIdentity.Structural);
+
+    let rec memoizedCheck x y =
+        let inner springs groups =
+            match springs with
+            | h::t ->
+                match Array.length groups = 0 with
+                | true ->
+                    match List.contains '#' springs with
+                    | true -> 0L
+                    | false -> 1L
+                | false -> 
+                    let groupSize = Array.head groups
+                    let validStretch =
+                        groupSize <= List.length springs &&
+                        List.take groupSize springs |> List.forall (fun c -> c <> '.') &&
+                        (groupSize = List.length springs || List.item groupSize springs <> '#')
+                    match h with
+                    | '.' -> memoizedCheck t groups
+                    | '?' when not validStretch-> memoizedCheck t groups
+                    | '?' when validStretch -> memoizedCheck t groups + memoizedCheck (trySkipWithDefault (groupSize + 1) springs) (Array.tail groups)
+                    | '#' when not validStretch -> 0L
+                    | '#' when validStretch -> memoizedCheck (trySkipWithDefault (groupSize + 1) springs) (Array.tail groups)
+                    | _ -> failwith "Springs should always match . ? or #"
+            | [] ->
+                match Array.length groups = 0 with
+                | true -> 1L
+                | false -> 0L
+        let k = (x |> List.map string |> String.concat "", y |> Array.map string |> String.concat "|")
+        match cache.TryGetValue k with
+        | true, v -> v
+        | _ -> 
+            let v = inner x y
+            cache.Add(k, v)
+            v
 
     let part1 (str : string) =
         parseInput str
         |> Array.fold (fun acc x ->
             let springs, groups = x
-            acc + check springs groups
-        ) 0
+            acc + memoizedCheck springs groups
+        ) 0L
 
     let part2 (str : string) =
-        -1
+        parseInput str
+        |> Array.map (fun x ->
+            let springs, groups = x
+            unfoldInput springs groups
+        )
+        |> Array.fold (fun acc x ->
+            let springs, groups = x
+            acc + memoizedCheck springs groups
+        ) 0L
